d918620913611e6b0c2f5ca7b07de112cfc0a65a
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-12-23 14:30:25 +0100

    poprawa 097

--- Zestaw_3:_Tablice_o_większej_liczbie_wymiarów/097/testy097.py
+++ Zestaw_3:_Tablice_o_większej_liczbie_wymiarów/097/testy097.py
@@ -35,43 +35,28 @@ def komenda(k: str, *args, **kwargs):
 class testy(unittest.TestCase):
 
     def test_Nr01_Zadanie_97_argumenty_tablica(self):
-        self.assertEqual(
-            Zadanie_97([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        )
+        self.assertEqual( Zadanie_97([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
     def test_Nr02_Zadanie_97_argumenty_tablica(self):
-        self.assertEqual(
-            Zadanie_97([[1, 2, 3], [2, 3, 4], [4, 5, 6]]), [1, 5, 6, 0, 0, 0, 0, 0, 0]
-        )
+        self.assertEqual( Zadanie_97([[1, 2, 3], [2, 3, 4], [4, 5, 6]]), [1, 5, 6, 0, 0, 0, 0, 0, 0])
 
     def test_Nr03_Zadanie_97_argumenty_tablica(self):
-        self.assertEqual(
-            Zadanie_97([[1, 1, 1], [1, 1, 1], [1, 1, 1]]), [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        )
+        self.assertEqual( Zadanie_97([[1, 1, 1], [1, 1, 1], [1, 1, 1]]), [0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     def test_Nr04_Zadanie_97_argumenty_tablica(self):
         self.assertEqual(Zadanie_97([[1]]), [1])
 
     def test_Nr05_Zadanie_97_argumenty_tablica(self):
-        self.assertEqual(
-            Zadanie_97([[1, 2, 5], [3, 4, 6], [5, 6, 7]]), [1, 2, 3, 4, 7, 0, 0, 0, 0]
-        )
+        self.assertEqual( Zadanie_97([[1, 2, 5], [3, 4, 6], [5, 6, 7]]), [1, 2, 3, 4, 7, 0, 0, 0, 0])
 
     def test_Nr06_Zadanie_97_argumenty_tablica(self):
-        self.assertEqual(
-            Zadanie_97([[100, 200, 300], [1000, 2000, 3000], [4000, 5000, 6000]]),
-            [100, 200, 300, 1000, 2000, 3000, 4000, 5000, 6000],
-        )
+        self.assertEqual( Zadanie_97([[100, 200, 300], [1000, 2000, 3000], [4000, 5000, 6000]]), [100, 200, 300, 1000, 2000, 3000, 4000, 5000, 6000],)
 
     def test_Nr07_Zadanie_97_argumenty_tablica(self):
-        self.assertEqual(
-            Zadanie_97([[1, 3, 5], [2, 4, 6], [7, 8, 9]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        )
+        self.assertEqual( Zadanie_97([[1, 3, 5], [2, 4, 6], [7, 8, 9]]), [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
     def test_Nr08_Zadanie_97_argumenty_tablica(self):
-        self.assertEqual(
-            Zadanie_97([[1000000, 999999999], [1, 2]]), [1, 2, 1000000, 999999999]
-        )
+        self.assertEqual( Zadanie_97([[1000000, 999999999], [1, 2]]), [1, 2, 1000000, 999999999])
 
     def test_Nr09_Zadanie_97_argumenty_tablica(self):
         self.assertEqual(Zadanie_97([[2115]]), [2115])
