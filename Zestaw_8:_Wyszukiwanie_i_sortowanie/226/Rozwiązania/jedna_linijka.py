15966760b8b09b1afe69574492714b101c65f99e
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-12-28 01:25:36 +0100

    jedna linijka 226

---
+++ Zestaw_8:_Wyszukiwanie_i_sortowanie/226/Rozwiązania/jedna_linijka.py
@@ -0,0 +1,8 @@
+def Zadanie_226(T):
+    T.sort(
+        key=lambda k: (
+            0
+            if {k[k.index(2) - 1], k[(k.index(2) + 1) % 4]} == {1, 3}
+            else 2 if {k[k.index(2) - 1], k[(k.index(2) + 1) % 4]} == {3, 4} else 1
+        )
+    )
